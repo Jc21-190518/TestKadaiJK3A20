3b9cd25ebbed49076b89705745f34932f81b77ee
翼 佐藤 <190518@jc-21.jp>
2022-02-15 14:21:28 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectedE.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKadai
+{
+    class DirectedE
+    {
+        public static int[] SEARCH(int[] edgeFirst, int[] edgeNext, int x)
+
+        {
+
+            int N = 6;
+
+            int M = 8;
+
+
+
+
+
+            int[] current = new int[N];
+
+            int[] searched = new int[M];
+
+            int[] path = new int[M];
+
+
+
+            int[] start = new int[] { 1, 2, 3, 4, 2, 5, 4, 6 };
+
+            int[] end = new int[] { 2, 3, 4, 1, 5, 4, 6, 2 };
+
+            for (int i = 0; i < N; i++)
+
+            {
+
+                current[i] = edgeFirst[i];
+
+            }
+
+
+
+            int top = 1;
+
+            int last = M;
+
+            int temp = 0;
+
+
+
+            while (last >= 1)
+
+            {
+
+                if (current[x - 1] != 0)
+
+                {
+
+                    temp = current[x - 1];
+
+                    searched[top - 1] = temp;
+
+                    current[x - 1] = edgeNext[temp - 1];
+
+                    x = end[temp - 1];
+
+
+
+                    top++;
+
+                }
+
+
+
+                else
+
+                {
+
+                    top--;
+
+
+
+                    temp = searched[top - 1];
+
+                    path[last - 1] = temp;
+
+                    x = start[temp - 1];
+
+
+
+                    last--;
+
+                }
+
+            }
+
+
+
+            return path;
+
+        }
+
+    }
+}
---
+++ Kuriage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKadai
+{
+    public class Kuriage
+    {
+
+        public static int kuriage10(int i)
+
+        {
+
+
+
+
+
+            if (i < 0)
+
+            {
+
+                i = i * -1;
+
+
+
+                int surplus = i % 10;
+
+                if (surplus != 0)
+
+                {
+
+                    i = (i - surplus) + 10;
+
+                }
+
+
+
+                i = i * -1;
+
+            }
+
+            else
+
+            {
+
+                int surplus = i % 10;
+
+                if (surplus != 0)
+
+                {
+
+                    i = (i - surplus) + 10;
+
+                }
+
+            }
+
+            return i;
+
+
+
+        }
+    }
+}
